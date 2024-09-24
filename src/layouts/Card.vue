b3122863a15411fd099806bc2729b0a782b9121a
ADRIAN <adriandeligero.1030@gmail.com>
2024-09-24 21:07:09 +0800

    fixed theme and added home section for client

--- src/layouts/Card.vue
+++ src/layouts/Card.vue
@@ -1,15 +1,35 @@
 <template>
     <div 
     class="p-5 height width rounded-3 mt"
+     :class="{'navbar-dark': isDark, 'navbar-light': !isDark}"
      :style="{ backgroundColor: isDark ? 'rgba(52, 52, 52, 0.8)' : 'rgba(200, 200, 200, 0.8)', color: isDark ? '#fff' : '#333' }"
     >
-    <h1 class="fw-bold">{{title}}</h1>
-    <h5>{{num}}</h5>
+    <h1 id="font" class="fw-bold">{{title}}</h1>
+    <h5 id="font">{{num}}</h5>
     </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'; 
+import { defineProps } from 'vue';
+import { useTheme } from 'vuetify';  
+
+onMounted(() => {
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    theme.global.name.value = savedTheme;
+    isDark.value = savedTheme === 'dark';
+  });
+
+    const theme = useTheme();
+  const isDark = ref(theme.global.current.value.dark); 
+  
+  const toggleTheme = () => {
+    const newTheme = isDark.value ? 'light' : 'dark';
+    theme.global.name.value = newTheme;
+    isDark.value = newTheme === 'dark';
+    localStorage.setItem('theme', newTheme); 
+  };
+  
+
 
 defineProps({
     title: {
@@ -32,5 +52,9 @@ margin-top: 100px;
 }
 .width{
     width: 450px;
+}
+#font {
+  font-size: 2rem;
+  font-family: "Merriweather", serif; 
 }
 </style>
