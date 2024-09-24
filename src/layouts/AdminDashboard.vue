54769cf88baa99f48d0dbac6ceaf09ba9eec4125
ADRIAN <adriandeligero.1030@gmail.com>
2024-09-24 10:18:42 +0800

    Added navbar but not finished

---
+++ src/layouts/AdminDashboard.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="w-100 d-flex h-100 gap-5">
+            <div 
+            class="w-25 h-100 p-5 "
+            :class="{'navbar-dark': isDark, 'navbar-light': !isDark}"
+            :style="{ backgroundColor: isDark ? 'rgba(52, 52, 52, 0.8)' : 'rgba(200, 200, 200, 0.8)', color: isDark ? '#fff' : '#333' }"
+            >
+            <div class=" d-flex align-items-center h-50 ">
+                <i class="fa-solid fa-house fa-lg"></i>
+                <router-link 
+                to="/Dashboard" 
+                class="btn wes fw-bold mt-1" 
+                :class="{ underline: route.path === '/Dashboard' }"
+                >Dashboard</router-link>
+            </div>
+        </div>
+        <div class="d-flex  justify-content-center gap-5 w-100 flex-wrap ">
+            <card title="Total Car in Sell"/>
+            <card title="Total Car in Rent"/>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+import Card from './Card.vue';
+
+
+const route = useRoute();
+</script>
+
+<style>
+
+.underline {
+    text-decoration: underline;
+}
+</style>
---
+++ src/layouts/Card.vue
@@ -0,0 +1,36 @@
+<template>
+    <div 
+    class="p-5 height width rounded-3 mt"
+     :style="{ backgroundColor: isDark ? 'rgba(52, 52, 52, 0.8)' : 'rgba(200, 200, 200, 0.8)', color: isDark ? '#fff' : '#333' }"
+    >
+    <h1 class="fw-bold">{{title}}</h1>
+    <h5>{{num}}</h5>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'; 
+
+defineProps({
+    title: {
+        type: String,
+        default: 'No title'
+    },
+    num: {
+        type: Number, 
+        default: 123,
+        validator: value => Number.isInteger(value) 
+    }
+});
+</script>
+<style>
+.mt{
+margin-top: 100px;
+}
+.height{
+    height: 150px;
+}
+.width{
+    width: 450px;
+}
+</style>
