e33f2221709b2b7e933af350fbf7e217c2953d60
whitelotus19 <centmarde.campado@carsu.edu.ph>
2024-10-14 02:59:46 +0800

    fix footer for tin2
    Co-Authored-By: C.M LAGURA <166703060+tinaylagura@users.noreply.github.com>
    Co-Authored-By: The Strongest Algorithm <159101935+centmarde@users.noreply.github.com>

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,47 +1,20 @@
 <template>
   <v-container fluid :class="{ 'dark-theme': !isDarkMode, 'light-theme': isDarkMode }">
-    <v-row>
-      <v-col cols="12" md="6">
-        <!-- Quick Links example -->
-        <h3 :class="{ 'white--text': !isDarkMode, 'black--text': isDarkMode }">Terms & Conditions</h3>
-        <div class="d-flex" >
-          <v-icon  :class="{ 'white--text': !isDarkMode, 'black--text': isDarkMode }">mdi-phone</v-icon> <h5 class="ms-5">09565545</h5>
-        </div>
-        <div class="d-flex" >
-          <v-icon :class="{ 'white--text': !isDarkMode, 'black--text': isDarkMode }">mdi-email</v-icon>
-        </div>
-        
-       
-      </v-col>
-
-
-      <v-col cols="12" md="6">
-        <!-- Social Links example -->
-        <h3 :class="{ 'white--text': !isDarkMode, 'black--text': isDarkMode }">Connect With Us</h3>
-        <div class="social-links">
-          <v-btn icon :href="'https://github.com/Centmarde'" target="_blank">
-            <v-icon :class="{ 'white--text': !isDarkMode, 'black--text': isDarkMode }">mdi-github</v-icon>
-          </v-btn>
-          <v-btn icon :href="'https://web.facebook.com/centmarde.campado'" target="_blank" class="ms-3">
-            <v-icon :class="{ 'white--text': !isDarkMode, 'black--text': isDarkMode }">mdi-facebook</v-icon>
-          </v-btn>
-        </div>
-      </v-col>
-    </v-row>
-
-
+    
      <!-- Social Links Row -->
      <v-row>
   <v-col cols="12" md="4" class="text-center">
     <!-- Team Image for Column 1 -->
     <v-img 
-      src="https://via.placeholder.com/100" 
+      src="https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/404955786_1494882281303982_3346562587604464766_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFx1tsJ6BAUhwCrUc77sJJ85SnoRYhXLpnlKehFiFcumfDP1NoAJJCdM8NelbRoQu_N6qApbxGmEkXTsJQyz8-F&_nc_ohc=0Mv_CkOf6OYQ7kNvgENFzD4&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=APAdgFuvw7mYqaUb5butM0F&oh=00_AYA8K5vp5c_oIFoPRobzp3jSxYEKzdNOdiAP_omODIvOAA&oe=6711E1D0" 
       aspect-ratio="1" 
       class="rounded-circle mx-auto mb-2" 
       width="100"
     ></v-img>
     <!-- Team Name for Column 1 -->
-    <div class="team-name" style="margin-bottom: 10px;">CAMPADO, CENTMARDE- Full Stack Developer</div>
+    <div class="team-name">CAMPADO, CENTMARDE - Future Full Stack Developer</div>
+<blockquote style="margin-top: -3px; padding: 0; font-style: italic;">"Creativity is a must, <br>but the way to solve new problems is ❤️."</blockquote>
+
 
     <!-- GitHub Icon -->
     <v-btn 
@@ -80,14 +53,16 @@
   <v-col cols="12" md="4" class="text-center">
      <!-- Team Image for Column 2 -->
      <v-img 
-      src="https://via.placeholder.com/100" 
+      src="https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/438305379_3828339190825649_1012384794768267985_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFe1DC2xmOCeIlLxQGFO1hGE_T0HIdzcdoT9PQch3Nx2nejLl2eDZDvgPI1zwvQgVZ9hNYYX6xClLhaPmRWOC6a&_nc_ohc=5_6qzeCHdLoQ7kNvgHk_lFl&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=AnMgXtCLK80wvCQPWrzN0vg&oh=00_AYAZzPlDyEjGbPashJnGPtv-Ro-m_v9daOfVvYLXjnfguA&oe=6711D484" 
       aspect-ratio="1" 
       class="rounded-circle mx-auto mb-2" 
       width="100"
     ></v-img>
     <!-- Team Name for Column 2 -->
      
-    <div class="team-name" style="margin-bottom: 10px;">DELIGERO, ADRIAN- Backend Engineer</div>
+    <div class="team-name">DELIGERO, ADRIAN- Backend Engineer</div>
+    <blockquote style="margin-top: -3px; padding: 0; font-style: italic;">"Empowering ideas through code and creativity."</blockquote>
+
 
     <!-- GitHub Icon -->
     <v-btn 
@@ -127,13 +102,15 @@
   <v-col cols="12" md="4" class="text-center">
      <!-- Team Image for Column 3 -->
      <v-img 
-      src="https://via.placeholder.com/100" 
+      src="https://avatars.githubusercontent.com/u/166703060?v=4" 
       aspect-ratio="1" 
       class="rounded-circle mx-auto mb-2" 
       width="100"
     ></v-img>
     <!-- Team Name for Column 3 -->
-    <div class="team-name" style="margin-bottom: 10px;">LAGURA, CHRISTINE MAE- System Analyst</div>
+    <div class="team-name" >LAGURA, CHRISTINE MAE- System Analyst</div>
+    <blockquote style="margin-top: -3px; padding: 0; font-style: italic;">"Empowering ideas through code and creativity."</blockquote>
+
 
     <!-- GitHub Icon -->
     <v-btn 
